478e1b877b490b03ef8124e72157ef15e6702b0e
Thomas Mc Ginley <A00217678@student.ait.ie>
2019-10-06 20:38:58 +0100

    Render Working

--- firstshaders/TmgRender.cpp
+++ firstshaders/TmgRender.cpp
@@ -1,22 +1,46 @@
 #pragma once
+#include <iostream>
+#include <sstream>
+#include <fstream>
 #include <GL/glew.h>
+#include <GL/freeglut.h>
 #include <glm/glm.hpp>
+#include <Windows.h>
 #include "TmgRender.h"
+
+using namespace std;
 using namespace glm;
 
 
-TmgRender::TMGRenderObject()
+void TmgRender::TMGRenderObject()
 {
 
 }
 
 bool TmgRender::createVboFromArray(vec3 vertexArray[], unsigned int size)
 {
+	numVerts = size;
+	cout << numVerts;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexArray), vertexArray, GL_STATIC_DRAW);
+	
+	return true;
 }
 
 void TmgRender::render() {
+	glClear(GL_COLOR_BUFFER_BIT);
 
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glDrawArrays(GL_TRIANGLES, 0, numVerts);
+
+	glDisableVertexAttribArray(0);
+
+	glutSwapBuffers();
 }
+
+//GLuint TmgRender::Getvbo() {
+//	return vbo;
+//}
--- firstshaders/TmgRender.h
+++ firstshaders/TmgRender.h
@@ -1,16 +1,21 @@
 #ifndef TmgRender_H
 #define	TmgRender_H
-
+#include <iostream>
+#include <sstream>
+#include <fstream>
 #include <GL/glew.h>
+#include <GL/freeglut.h>
 #include <glm/glm.hpp>
+#include <Windows.h>
 using namespace glm;
 
 class TmgRender
 {
 public:
-	TMGRenderObject();
+	void TMGRenderObject();
 	bool createVboFromArray(vec3 vertexArray[], unsigned int size);
 	void render();
+	//GLuint Getvbo();
 
 private:
 	GLuint vbo;
